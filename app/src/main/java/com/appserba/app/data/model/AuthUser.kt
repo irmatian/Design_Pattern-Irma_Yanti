66a9c7bfe5d403a8bb0444b9026f7ef32433853f
yogiyulianto <irma.yanti@students.amikom.ac.id>
2020-12-11 23:23:15 +0700

    design pattern apserba

---
+++ app/src/main/java/com/appserba/app/data/model/AuthUser.kt
@@ -0,0 +1,11 @@
+package com.appserba.app.data.model
+
+import java.net.PasswordAuthentication
+
+data class AuthUser (
+    val id: Int = 0,
+    val email: String = "",
+    val password: String = "",
+    val fullName: String = ""
+
+    )
